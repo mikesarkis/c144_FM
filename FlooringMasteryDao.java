54fa5596c269e0ff7d7fb932d76c2f585731114f
Mikaeil Sarkis <59941108+mikesarkis@users.noreply.github.com>
2021-11-26 18:51:42 -0500

    Update FlooringMasteryDao.java

--- FlooringMasteryDao.java
+++ FlooringMasteryDao.java
@@ -6,6 +6,10 @@
 package com.mycompany.flooringmastery.dao;
 
 import com.mycompany.flooringmastery.dto.Order;
+import com.mycompany.flooringmastery.dto.Product;
+import com.mycompany.flooringmastery.dto.Tax;
+import java.io.FileNotFoundException;
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -13,9 +17,11 @@ import java.util.List;
  * @author Mike
  */
 public interface FlooringMasteryDao {
-    public Order addOrder(int number, Order order);
+     public Order addOrder(String date, String name, String State, BigDecimal taxrate, String type, BigDecimal area, BigDecimal costperfoot,BigDecimal laborcostperfoot);
     public List<Order> get_all_orders();
     public Order remove_order(int number);
-    public Order edit_order(int number , String data, int choice);
+    public Order edit_order(int number,Order copy);
+    public Order getOrder(int number);
+
     
 }
