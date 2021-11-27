693753cfafed630cbd42c2dddcf1cb44489601b0
Mikaeil Sarkis <59941108+mikesarkis@users.noreply.github.com>
2021-11-27 17:09:11 -0500

    Update FlooringMasteryServiceLayerFileImpl.java

--- Service/FlooringMasteryServiceLayerFileImpl.java
+++ Service/FlooringMasteryServiceLayerFileImpl.java
@@ -5,6 +5,7 @@
  */
 package com.mycompany.flooringmastery.serivce;
 
+import com.mycompany.flooringmastery.dao.FlooringMasteryDao;
 import com.mycompany.flooringmastery.dao.FlooringMasteryDaoImpl;
 import static com.mycompany.flooringmastery.dao.FlooringMasteryDaoImpl.DELIMITER;
 import com.mycompany.flooringmastery.dto.Order;
@@ -19,52 +20,78 @@ import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  *
  * @author Mike
  */
+@Component
 public class FlooringMasteryServiceLayerFileImpl implements FlooringMasteryServiceLayer{
-    List<FlooringMasteryDaoImpl>list;
-    private final String TAXFILE= "Taxes.txt";;
+    FlooringMasteryDao dao; 
+    private final String TAXFILE= "Taxes.txt";
     private final String PRODUCTFILE = "Products.txt";
-    public FlooringMasteryServiceLayerFileImpl()
+    @Autowired
+    public FlooringMasteryServiceLayerFileImpl(FlooringMasteryDaoImpl dao)
     {
-        list =  new ArrayList<>();
-    }
+        this.dao = dao;
+    } 
 
-      private boolean validatefile(String filename) throws NodateFoundException
+      private boolean validateOrderFile(String filename) throws NoorderFoundException, IOException
       {
           boolean found = false;
-            for(int i=0;i<list.size();i++)
-                {
-                    if(list.get(i).getfileName().equals(filename))
-                    {
-                        
-                       found = true;
-                    }
-                }
+          dao.setFileName(filename);
+          List<Order> list = dao.get_all_orders();
+          if(list.size()>0)
+          {
+              found = true;
+          }
           try{  if( found == true)
             {
                 return found;
             }
           else
-          throw new NodateFoundException("no order found on this date");
-          }catch (NodateFoundException e)
+          throw new NoorderFoundException("no order found on this date");
+          }catch (NoorderFoundException e)
           {
               System.out.println(e.getMessage());
                return false;
           }
            
       }
+      private boolean validateSpecificOrder(int number, String filename) throws IOException, NoorderFoundException, NospecificorderException
+      {
+          boolean found = false;
+          dao.setFileName(filename);
+          List<Order> list = dao.get_all_orders();
+          for(int i=0 ; i< list.size();i++)
+              {
+                 if(list.get(i).get_order_number() == number) 
+                 {
+                     found = true;
+                 }
+              }
+          try{  if( found == true)
+            {
+                return found;
+            }
+          else
+          throw new NospecificorderException("this specific order was not  found on this date");
+          }catch (NospecificorderException e)
+          {
+              System.out.println(e.getMessage());
+               return false;
+          }
+          
+      
+      }
     private String fixdate(String date)
     {
-        String date2 = date.replace("/","");
+        String date2 = date.replace("-","");
         return date2;
     }
-    private int getlocation(List<FlooringMasteryDaoImpl>list, String filename)
+ /*   private int getlocation(List<FlooringMasteryDaoImpl>list, String filename)
     {
         boolean isfound = false;
         int index=0;
@@ -82,75 +109,60 @@ public class FlooringMasteryServiceLayerFileImpl implements FlooringMasteryServi
         }
         else
             return -1;
-    }
+    } */
     @Override
-    public Order addorder(String date, String name, String State, BigDecimal taxrate, String type, BigDecimal area, BigDecimal costperfoot,BigDecimal laborcostperfoot) throws NodateFoundException{
+    public Order addorder(String date, String name, String State, BigDecimal taxrate, String type, BigDecimal area, BigDecimal costperfoot,BigDecimal laborcostperfoot) throws IOException {
         String fileName;
         String datefixed =  fixdate(date);
-        Order temp;
-        temp = new Order(1, date);
         fileName = "Orders_"+datefixed+".txt";
-        int index = getlocation(list, fileName);
-        if(index < 0)
-           {
-             try {
-                    FlooringMasteryDaoImpl dao1 = new FlooringMasteryDaoImpl(fileName);
-                    dao1.addOrder(date,name,State,taxrate,type,area,costperfoot, laborcostperfoot);
-                    list.add(dao1);
-                 } catch (IOException ex) {
-                        Logger.getLogger(FlooringMasteryServiceLayerFileImpl.class.getName()).log(Level.SEVERE, null, ex);
-                 }
-                  return temp;
-              }
-            if (index >=0) {
-                list.get(index).addOrder(date,name,State,taxrate,type,area,costperfoot, laborcostperfoot);
-              }
-
-
-       
+        Order temp;
+        dao.setFileName(fileName);
+        temp = dao.addOrder(date,name,State,taxrate,type,area,costperfoot, laborcostperfoot);
         return temp;
     }
 
     @Override
-    public Order editOrder(int number, String date, Order copy) throws  NodateFoundException{
+    public Order editOrder(int number, String date, Order copy) throws IOException, NoorderFoundException , NospecificorderException{
         String fileName;
         String datefixed =  fixdate(date);
         fileName = "Orders_"+datefixed+".txt";
+        dao.setFileName(fileName);
         Order temp;
-        if( validatefile(fileName))
+        if( validateOrderFile(fileName)&&validateSpecificOrder(number, fileName))
             {
-               int index = getlocation(list, fileName);
-              temp = list.get(index).edit_order(number, copy);
+               
+             temp =  dao.edit_order(number, copy);
             }
         else
-            temp = null;
+            temp= null;
+        
         return temp;
 
     }
 
     @Override
-    public void removeOrder(int number, String date) throws NodateFoundException{
+    public void removeOrder(int number, String date)  throws IOException, NoorderFoundException , NospecificorderException{
         String fileName;
         String datefixed =  fixdate(date);
         fileName = "Orders_"+datefixed+".txt";
-        if( validatefile(fileName))
+        dao.setFileName(fileName);
+        if( validateOrderFile(fileName)&&validateSpecificOrder(number, fileName))
             {
-               int index = getlocation(list, fileName);
-               list.get(index).remove_order(number);
+               dao.remove_order(number);
             }
-
     }
 
     @Override
-    public Order getOrder(int number, String date) throws  NodateFoundException{
+    public Order getOrder(int number, String date) throws IOException, NoorderFoundException , NospecificorderException{
         String fileName;
         String datefixed =  fixdate(date);
         fileName = "Orders_"+datefixed+".txt";
+        dao.setFileName(fileName);
         Order temp;
-        if( validatefile(fileName))
+        if( validateOrderFile(fileName)&&validateSpecificOrder(number, fileName))
             {
-               int index = getlocation(list, fileName);
-               temp = list.get(index).getOrder(number);
+               
+               temp = dao.getOrder(number);
             }
         else
             temp = null;
@@ -158,18 +170,20 @@ public class FlooringMasteryServiceLayerFileImpl implements FlooringMasteryServi
     }
 
     @Override
-    public List<Order> getOrders(String date) throws  NodateFoundException{
+    public List<Order> getOrders(String date) throws  NoorderFoundException, IOException{
         String fileName;
         String datefixed =  fixdate(date);
         fileName = "Orders_"+datefixed+".txt";
+        FlooringMasteryDaoImpl dao1;
+        dao.setFileName(fileName);
         List<Order> List_temp;
-        if(validatefile(fileName))
+        if(validateOrderFile(fileName))
             {
-               int index = getlocation(list, fileName);
-               List_temp = list.get(index).get_all_orders();
+               List_temp = dao.get_all_orders();
             }
         else
             List_temp = null;
+        
         return List_temp;
     }
 
@@ -228,6 +242,7 @@ public class FlooringMasteryServiceLayerFileImpl implements FlooringMasteryServi
         BigDecimal LaborCostPerSquareFoot = b.setScale(2, RoundingMode.HALF_UP);
         return new Product(productname,CostPerSquareFoot,LaborCostPerSquareFoot);
     }
+    @Override
     public List<Product> getallproduct() throws FileNotFoundException
     {
         List<Product> list1 = new ArrayList<>();
@@ -245,6 +260,7 @@ public class FlooringMasteryServiceLayerFileImpl implements FlooringMasteryServi
          scan.close();
          return list1;
     }
+    @Override
     public BigDecimal getcost(String type) throws FileNotFoundException
     {
         List<Product> list1 = getallproduct();
@@ -273,6 +289,16 @@ public class FlooringMasteryServiceLayerFileImpl implements FlooringMasteryServi
         }
         
       return list1.get(index).getLaborCostPerSquareFoot();
+    }
+
+    @Override
+    public void exportData(String date) throws FileNotFoundException, IOException{
+        String fileName;
+        String datefixed =  fixdate(date);
+        fileName = "Orders_"+datefixed+".txt";
+        dao.setFileName(fileName);
+        dao.exportData();
+        
     }
         
 
